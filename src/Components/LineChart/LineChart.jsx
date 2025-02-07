7ce1ae5308b8ed54d37c80c144bf8e8c5ea11d20
anikatasnin <jarintasnin09@gmail.com>
2025-02-07 21:50:42 +0600

    updated linechart.jsx

--- src/Components/LineChart/LineChart.jsx
+++ src/Components/LineChart/LineChart.jsx
@@ -1,18 +1,22 @@
-import { LineChart as LChart, Line } from "recharts";
+import { LineChart as LChart, Line, XAxis, YAxis, Tooltip } from "recharts";
 
 const LineChart = () => {
   const servicePercentage = [
-    { id: 1, name: "Facials", num: 80 },
-    { id: 2, name: "Haircuts", num: 90 },
-    { id: 3, name: "Makeup", num: 60 },
-    { id: 4, name: "Manicure & Pedicure", num: 50 },
-    { id: 5, name: "Waxing", num: 80 },
+    { id: 1, name: "Facials", percentage: 80 },
+    { id: 2, name: "Haircuts", percentage: 90 },
+    { id: 3, name: "Makeup", percentage: 60 },
+    { id: 4, name: "Manicure & Pedicure", percentage: 50 },
+    { id: 5, name: "Waxing", percentage: 80 },
   ];
 
   return (
     <div>
-      <LChart width={500} height={400} data={servicePercentage}></LChart>
-      <Line></Line>
+      <LChart width={500} height={400} data={servicePercentage}>
+        <XAxis dataKey="name"></XAxis>
+        <YAxis dataKey="percentage"></YAxis>
+        <Tooltip />
+        <Line dataKey="percentage" stroke="green"></Line>
+      </LChart>
     </div>
   );
 };
