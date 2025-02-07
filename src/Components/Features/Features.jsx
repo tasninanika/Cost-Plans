48d281d67e7c1b1ccfac5a92e49d596da58d67c6
anikatasnin <jarintasnin09@gmail.com>
2025-02-07 21:17:39 +0600

    updated navbar2

--- src/Components/Features/Features.jsx
+++ src/Components/Features/Features.jsx
@@ -1,11 +1,19 @@
 import PropTypes from "prop-types";
+import { FaCircleCheck } from "react-icons/fa6";
 
 const Features = ({ feature }) => {
-  return <div></div>;
+  return (
+    <div>
+      <p className="flex items-center gap-2">
+        <FaCircleCheck className="text-green-800" />
+        {feature}
+      </p>
+    </div>
+  );
 };
 
-Features.PropTypes = {
-  feature: PropTypes.object,
+Features.propTypes = {
+  feature: PropTypes.string,
 };
 
 export default Features;
