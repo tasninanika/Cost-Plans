3851c4fabcd971ba889bad9e5cb502062f7bf423
anikatasnin <jarintasnin09@gmail.com>
2025-02-07 21:10:24 +0600

    updated navbar2

---
+++ src/Components/Features/Features.jsx
@@ -0,0 +1,11 @@
+import PropTypes from "prop-types";
+
+const Features = ({ feature }) => {
+  return <div></div>;
+};
+
+Features.PropTypes = {
+  feature: PropTypes.object,
+};
+
+export default Features;
--- src/Components/PriceOption/PriceOption.jsx
+++ src/Components/PriceOption/PriceOption.jsx
@@ -1,19 +1,23 @@
 import PropTypes from "prop-types";
+import Features from "../Features/Features";
 
 const PriceOption = ({ option }) => {
   const { name, price, features } = option;
   return (
-    <div className="bg-blue-100">
-      <h2>
-        <span className="text-5xl">{price}</span>
+    <div className="bg-yellow-100 rounded-lg p-8">
+      <h2 className="text-center">
+        <span className="text-5xl font-semibold">{price}</span>
         <span className="text-2xl">$</span>
       </h2>
-      <h4 className="text-3xl">{name}</h4>
+      <h4 className="text-3xl text-center">{name}</h4>
+      {features.map((feature, index) => (
+        <Features key={index} feature={feature}></Features>
+      ))}
     </div>
   );
 };
 
-PriceOption.PropTypes = {
+PriceOption.propTypes = {
   option: PropTypes.object,
 };
 
