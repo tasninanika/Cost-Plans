9b0b957f169deed7f933485d616f85a945c3f096
anikatasnin <jarintasnin09@gmail.com>
2025-02-07 21:41:55 +0600

    updated linechart.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import LineChart from "./Components/LineChart/LineChart";
 // import Navbar from './Components/Navbar/Navbar'
 import Navbar2 from "./Components/Navbar2/Navbar2";
 import PriceOptions from "./Components/PriceOptions/PriceOptions";
@@ -9,6 +10,7 @@ function App() {
       {/* <Navbar></Navbar> */}
       <Navbar2></Navbar2>
       <PriceOptions></PriceOptions>
+      <LineChart></LineChart>
     </>
   );
 }
--- src/Components/LineChart/LineChart.jsx
+++ src/Components/LineChart/LineChart.jsx
@@ -1,5 +1,20 @@
+import { LineChart as LChart, Line } from "recharts";
+
 const LineChart = () => {
-  return <div></div>;
+  const servicePercentage = [
+    { id: 1, name: "Facials", num: 80 },
+    { id: 2, name: "Haircuts", num: 90 },
+    { id: 3, name: "Makeup", num: 60 },
+    { id: 4, name: "Manicure & Pedicure", num: 50 },
+    { id: 5, name: "Waxing", num: 80 },
+  ];
+
+  return (
+    <div>
+      <LChart width={500} height={400} data={servicePercentage}></LChart>
+      <Line></Line>
+    </div>
+  );
 };
 
 export default LineChart;
