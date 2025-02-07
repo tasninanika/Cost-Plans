ab1b97515dae29deb9e28e55c225bee3982d80ed
anikatasnin <jarintasnin09@gmail.com>
2025-02-07 22:03:42 +0600

    updated linechart.jsx

--- src/Components/LineChart/LineChart.jsx
+++ src/Components/LineChart/LineChart.jsx
@@ -5,18 +5,25 @@ const LineChart = () => {
     { id: 1, name: "Facials", percentage: 80 },
     { id: 2, name: "Haircuts", percentage: 90 },
     { id: 3, name: "Makeup", percentage: 60 },
-    { id: 4, name: "Manicure & Pedicure", percentage: 50 },
+    { id: 4, name: "Manicure & Pedicure", percentage: 100 },
     { id: 5, name: "Waxing", percentage: 80 },
   ];
 
   return (
     <div>
-      <LChart width={500} height={400} data={servicePercentage}>
-        <XAxis dataKey="name"></XAxis>
-        <YAxis dataKey="percentage"></YAxis>
-        <Tooltip />
-        <Line dataKey="percentage" stroke="green"></Line>
-      </LChart>
+      <div>
+        <h2 className="text-center text-4xl font-semibold my-20">
+          Beauty Trends Insights
+        </h2>
+      </div>
+      <div className="flex justify-center">
+        <LChart width={500} height={400} data={servicePercentage}>
+          <XAxis dataKey="name"></XAxis>
+          <YAxis dataKey="percentage"></YAxis>
+          <Tooltip />
+          <Line dataKey="percentage" stroke="green"></Line>
+        </LChart>
+      </div>
     </div>
   );
 };
