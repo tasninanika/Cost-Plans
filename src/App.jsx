5063169612d07faf5f30a66d7af2eb6bda0c3a39
anikatasnin <jarintasnin09@gmail.com>
2025-02-07 12:24:47 +0600

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import Navbar2 from './Components/Navbar2/Navbar2'
 
 function App() {
 
   return (
     <>
       <Navbar></Navbar>
+      <Navbar2></Navbar2>
     </>
   )
 }
---
+++ src/Components/Navbar2/Navbar2.jsx
@@ -0,0 +1,10 @@
+
+const Navbar2 = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Navbar2;
