6505391ceb56efa0ab480b59d1672a55111ae06e
anikatasnin <jarintasnin09@gmail.com>
2025-02-07 18:21:04 +0600

    updated navbar2

--- src/Components/PriceOption/PriceOption.jsx
+++ src/Components/PriceOption/PriceOption.jsx
@@ -1,7 +1,16 @@
 import PropTypes from "prop-types";
 
 const PriceOption = ({ option }) => {
-  return <div></div>;
+  const { name, price, features } = option;
+  return (
+    <div>
+      <h2>
+        <span className="text-5xl">{price}</span>
+        <span className="text-2xl">$</span>
+      </h2>
+      <h4 className="text-3xl">{name}</h4>
+    </div>
+  );
 };
 
 PriceOption.PropTypes = {
