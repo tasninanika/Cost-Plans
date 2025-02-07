68e637e96b1833b8d56311e5465f43d54313a97a
anikatasnin <jarintasnin09@gmail.com>
2025-02-07 13:03:36 +0600

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
-import Navbar from './Components/Navbar/Navbar'
+// import Navbar from './Components/Navbar/Navbar'
 import Navbar2 from './Components/Navbar2/Navbar2'
 
 function App() {
 
   return (
     <>
-      <Navbar></Navbar>
+      {/* <Navbar></Navbar> */}
       <Navbar2></Navbar2>
     </>
   )
--- src/Components/Navbar2/Navbar2.jsx
+++ src/Components/Navbar2/Navbar2.jsx
@@ -1,9 +1,24 @@
 
 const Navbar2 = () => {
+
+    const routes = [
+        { id: 1, path: "/", name: "Home" },
+        { id: 2, path: "/about", name: "About" },
+        { id: 3, path: "/services", name: "Services" },
+        { id: 4, path: "/pricing", name: "Pricing" },
+        { id: 5, path: "/contact", name: "Contact" }
+      ];
+      
+      
+
     return (
-        <div>
-            
-        </div>
+        <nav>
+            <ul className="md:flex">
+            {
+                routes.map(route => <li className="mr-10" key={route.id}> <a href={`route.path`}>{route.name}</a></li>)
+            }
+            </ul>
+        </nav>
     );
 };
 
