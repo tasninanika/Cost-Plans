e25b13f17c6fd91c40b037cb375f44e1b70a515a
anikatasnin <jarintasnin09@gmail.com>
2025-02-07 17:26:58 +0600

    added navbar2

--- src/Components/Link/Link.jsx
+++ src/Components/Link/Link.jsx
@@ -1,11 +1,17 @@
+import PropTypes from "prop-types";
+
 const Link = ({ route }) => {
   return (
     <div>
-      <li className="mr-10" key={route.id}>
-        <a href={`route.path`}>{route.name}</a>
+      <li className="mr-10">
+        <a href={route.path}>{route.name}</a>
       </li>
     </div>
   );
+};
+
+Link.propTypes = {
+  route: PropTypes.object,
 };
 
 export default Link;
