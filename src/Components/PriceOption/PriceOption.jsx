e7c4c22ee7b99cf0d5863e64b4aed4cd80fd836a
anikatasnin <jarintasnin09@gmail.com>
2025-02-07 21:22:27 +0600

    updated navbar2

--- src/Components/PriceOption/PriceOption.jsx
+++ src/Components/PriceOption/PriceOption.jsx
@@ -4,15 +4,19 @@ import Features from "../Features/Features";
 const PriceOption = ({ option }) => {
   const { name, price, features } = option;
   return (
-    <div className="bg-yellow-100 rounded-lg p-8">
+    <div className="bg-yellow-100 rounded-xl p-8">
       <h2 className="text-center">
         <span className="text-5xl font-semibold">{price}</span>
         <span className="text-2xl">$</span>
       </h2>
-      <h4 className="text-3xl text-center">{name}</h4>
-      {features.map((feature, index) => (
-        <Features key={index} feature={feature}></Features>
-      ))}
+      <h4 className="text-3xl text-center mt-5 text-green-900 font-bold">
+        {name}
+      </h4>
+      <div className="mt-8">
+        {features.map((feature, index) => (
+          <Features key={index} feature={feature}></Features>
+        ))}
+      </div>
     </div>
   );
 };
